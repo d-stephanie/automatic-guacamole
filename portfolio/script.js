109d805fd5db1729e4164bc82543888c65cf0299
d-stephanie <113816446+d-stephanie@users.noreply.github.com>
2022-09-22 18:12:23 +0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,36 @@
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+    accordion.onclick = function() {
+        this.classList.toggle("is-open");
+
+        let content = this.nextElementSibling;
+        console.log(content);
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            console.log(content.style.maxHeight);
+        }
+
+    };
+});
+
+
+document.getElementById("guessing").addEventListener("click", function(event) {
+
+    const question = prompt("Kompakysymys loppuun, mikä on virossa ja ruotsissa, mutta ei saksassa ja suomessa?", "vastaus...")
+
+    if (question == "S-kirjain") {
+        document.getElementById("guess").innerHTML = "OIKEIN!";
+    } else if (question == "s-kirjain") {
+        document.getElementById("guess").innerHTML = "OIKEIN!";
+    } else {
+        document.getElementById("guess").innerHTML = "Väärin... oikea vastaus olisi ollut S-kirjain.";
+    };
+
+    setTimeout(function() {
+        document.getElementById("guess").innerHTML = "";
+    }, 1000);
+});
